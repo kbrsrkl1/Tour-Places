67413a07729015fe20701d3a6933e579c3e570c8
kbrsrkl1 <kubranur216@gmail.com>
2023-06-27 14:10:58 +0300

    commit

--- solution/src/App.js
+++ solution/src/App.js
@@ -1,20 +1,15 @@
 import './App.scss';
 import Navbar from './component/Navbar/navbar';
-import Header from './component/Header/header';
-import Main from './component/Main/main';
 
-import React from 'react'
+
 
 const App = () => {
-  return (
+  return(
     <div className="App">
       <Navbar/>
-      <Header/>
-      <Main/>
-    </div>
+    </div> 
   )
 }
 
-export default App
 
-
+export default App;
